c72552f7310105be479162f7a106aa064e9d091b
NikolayLiemiekhov <nicklemster@gmail.com>
2022-01-04 17:50:43 +0200

    1:07:58 Move FirebaseState to firebase folder.

--- src/context/firebase/FirebaseState.js
+++ src/context/firebase/FirebaseState.js
@@ -1,7 +1,7 @@
 import React, {useReducer} from "react";
 import axios from "axios";
-import {FirebaseContext} from "../firebase/firebaseContext";
-import {firebaseReducer} from "../firebase/firebaseReducer";
+import {FirebaseContext} from "./firebaseContext";
+import {firebaseReducer} from "./firebaseReducer";
 import {ADD_NOTE, FETCH_NOTES, REMOVE_NOTE, SHOW_LOADER} from "../types";
 
 const url = process.env.REACT_APP_DB_URL
