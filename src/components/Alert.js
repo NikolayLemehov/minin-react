6b619aa5a7896c91d3d967ca937c4b2a9000735e
NikolayLiemiekhov <nicklemster@gmail.com>
2022-01-04 20:13:11 +0200

    1:15:30 Fixes animation timeout for Alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -7,7 +7,10 @@ export const Alert = () => {
   return (
     <CSSTransition
       in={alert.visible}
-      timeout={750}
+      timeout={{
+        enter: 500,
+        exit: 350,
+      }}
       classNames={'alert'}
       mountOnEnter
       unmountOnExit
