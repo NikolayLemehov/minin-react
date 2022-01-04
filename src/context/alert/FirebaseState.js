23c51f624ed7b670392b38a5eaa0e95dae2ea880
NikolayLiemiekhov <nicklemster@gmail.com>
2022-01-04 17:08:24 +0200

    1:05:25 Add dispatch with FETCH_NOTES and ADD_NOTE to FirebaseState.js

--- src/context/alert/FirebaseState.js
+++ src/context/alert/FirebaseState.js
@@ -2,7 +2,7 @@ import React, {useReducer} from "react";
 import axios from "axios";
 import {FirebaseContext} from "../firebase/firebaseContext";
 import {firebaseReducer} from "../firebase/firebaseReducer";
-import {REMOVE_NOTE, SHOW_LOADER} from "../types";
+import {ADD_NOTE, FETCH_NOTES, REMOVE_NOTE, SHOW_LOADER} from "../types";
 
 const url = process.env.REACT_APP_DB_URL
 export const FirebaseState = ({children}) => {
@@ -16,6 +16,13 @@ export const FirebaseState = ({children}) => {
     showLoader()
     const res = await axios.get(`${url}/notes.json`)
     console.log('fetchNotes', res.data)
+    const payload = Object.keys(res.data).map(key => {
+      return {
+        ...res.data[key],
+        id: key,
+      }
+    })
+    dispatch({type: FETCH_NOTES, payload})
   }
   const addNote = async title => {
     const note = {
@@ -23,7 +30,11 @@ export const FirebaseState = ({children}) => {
     }
     try {
       const res = await axios.post(`${url}/notes.json`, note)
-      console.log('addNote', res.data)
+      const payload = {
+        ...note,
+        id: res.data.name,
+      }
+      dispatch({type: ADD_NOTE, payload})
     } catch (e) {
       throw new Error(e.message)
     }
